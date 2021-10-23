56155bdaeafbbdaa2ff88a8782270cbddfea9ef4
m-cartmell <hello@mcartmell.com>
2021-10-23 12:47:08 +0100

    Adjust CSP blockages

--- csp.js
+++ csp.js
@@ -10,19 +10,20 @@ const src = {
   _7: '*.elfsight.com',
   _8: 'https://proxy.elfsightcdn.com',
   _9: 'https://api.instacloud.io',
+  _10: 'https://vitals.vercel-insights.com',
 };
 
-const { _1, _2, _3, _4, _5, _6, _7, _8, _9 } = src;
+const { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10 } = src;
 
 exports.csp = () => {
   let directive = `default-src 'self';`;
   directive += `script-src 'self' 'unsafe-inline' ${
     dev ? `'unsafe-eval'` : ''
-  } ${_1} ${_2} ${_3} ${_5} ${_7};`;
+  } ${_1} ${_2} ${_3} ${_5} ${_7} ${_8};`;
   directive += `style-src 'self' 'unsafe-inline' ${_4};`;
   directive += `font-src 'self' ${_5};`;
   directive += `frame-src 'self' ${_1};`;
-  directive += `connect-src 'self' ${_2} ${_7} ${_9};`;
+  directive += `connect-src 'self' ${_2} ${_7} ${_9} ${_10};`;
   directive += `img-src 'self' ${_2} ${_6} ${_8};`;
 
   return directive;
