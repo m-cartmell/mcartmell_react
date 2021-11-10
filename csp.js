b07cd940eff2e4db3c105494bbbbf7dda771e1a9
m-cartmell <hello@mcartmell.com>
2021-11-10 12:40:13 +0000

    CSP adjustments

--- csp.js
+++ csp.js
@@ -5,16 +5,14 @@ const src = {
   _2: 'https://www.google-analytics.com',
   _3: 'https://www.googletagmanager.com',
   _4: 'https://fonts.googleapis.com',
-  _5: '*.gstatic.com',
+  _5: 'https://*.gstatic.com',
   _6: 'data:',
-  _7: '*.elfsight.com',
-  _8: 'https://proxy.elfsightcdn.com',
-  _9: 'https://api.instacloud.io',
-  _10: 'https://vitals.vercel-insights.com',
-  _11: 'https://*.cdninstagram.com',
+  _7: 'https://*.elfsight.com',
+  _8: 'https://vitals.vercel-insights.com',
+  _9: 'https://*.cdninstagram.com',
 };
 
-const { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11 } = src;
+const { _1, _2, _3, _4, _5, _6, _7, _8, _9 } = src;
 
 exports.csp = () => {
   let directive = `default-src 'self';`;
@@ -24,8 +22,8 @@ exports.csp = () => {
   directive += `style-src 'self' 'unsafe-inline' ${_4};`;
   directive += `font-src 'self' ${_5};`;
   directive += `frame-src 'self' ${_1};`;
-  directive += `connect-src 'self' ${_2} ${_7} ${_8} ${_9} ${_10};`;
-  directive += `img-src 'self' ${_2} ${_6} ${_8} ${_11};`;
+  directive += `connect-src 'self' ${_2} ${_7} ${_8};`;
+  directive += `img-src 'self' ${_2} ${_6} ${_9};`;
 
   return directive;
 };
