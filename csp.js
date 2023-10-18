cbd0b17c147dfe58d1b8492af58c814493342168
m-cartmell <hello@mcartmell.com>
2023-10-18 18:11:57 +0100

    Another CSP updates for google-analytics

--- csp.js
+++ csp.js
@@ -2,8 +2,8 @@ const dev = process.env.NODE_ENV === 'development';
 
 const src = {
   _1: 'https://www.google.com',
-  _2: 'https://www.google-analytics.com',
-  _3: 'https://*.googletagmanager.com',
+  _2: 'https://*.google-analytics.com',
+  _3: 'https://www.googletagmanager.com',
   _4: 'https://fonts.googleapis.com',
   _5: 'https://*.gstatic.com',
   _6: 'data:',
