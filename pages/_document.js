db0238368d4cf994de8d288d2a27f2778365d456
m-cartmell <hello@mcartmell.com>
2023-10-18 17:52:19 +0100

    Set up GA4

--- pages/_document.js
+++ pages/_document.js
@@ -6,19 +6,21 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ID}`}
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
           />
           <script
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                });
+                `,
             }}
           />
         </Head>
