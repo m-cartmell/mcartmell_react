cdcbe0f87438ffc5ef62e946988462c44ebae1e0
m-cartmell <hello@mcartmell.com>
2024-12-20 16:20:45 +0000

    More CSP updates

--- csp.js
+++ csp.js
@@ -11,9 +11,12 @@ const src = {
   _8: 'https://vitals.vercel-insights.com',
   _9: 'https://*.cdninstagram.com',
   _10: 'https://*.elfsightcdn.com',
+  _11: 'https://stats.g.doubleclick.net',
+  _12: 'https://www.google.co.uk',
+  _13: 'https://td.doubleclick.net',
 };
 
-const { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10 } = src;
+const { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13 } = src;
 
 exports.csp = () => {
   let directive = `default-src 'self';`;
@@ -22,9 +25,9 @@ exports.csp = () => {
   } ${_1} ${_2} ${_3} ${_5} ${_7} ${_9} ${_10};`;
   directive += `style-src 'self' 'unsafe-inline' ${_4};`;
   directive += `font-src 'self' ${_5};`;
-  directive += `frame-src 'self' ${_1};`;
-  directive += `connect-src 'self' ${_2} ${_7} ${_8};`;
-  directive += `img-src 'self' ${_2} ${_6} ${_9} ${_10};`;
+  directive += `frame-src 'self' ${_1} ${_13};`;
+  directive += `connect-src 'self' ${_2} ${_7} ${_8} ${_11};`;
+  directive += `img-src 'self' ${_2} ${_6} ${_9} ${_10} ${_12};`;
 
   return directive;
 };
