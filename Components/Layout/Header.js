89eb58bd49f493c9acc16003b78a5e925766b4eb
m-cartmell <hello@mcartmell.com>
2026-04-17 00:46:49 +0100

    Upated headings and favicons

--- Components/Layout/Header.js
+++ Components/Layout/Header.js
@@ -7,26 +7,15 @@ export default function Header() {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <meta name="apple-mobile-web-app-title" content="mcartmell" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a6acaf" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <header className={styles.header}>
