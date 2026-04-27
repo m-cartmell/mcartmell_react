1777333e3c70fc7555f8fc3ad0f264d9f1c75181
m-cartmell <hello@mcartmell.com>
2026-04-27 07:19:48 +0100

    Switched to next/font

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,30 @@ import Layout from '../Components/Layout';
 import { Provider } from '../Components/Context';
 import '../scss/globals.scss';
 import { Analytics } from '@vercel/analytics/next';
+import { Montserrat, Plus_Jakarta_Sans } from 'next/font/google';
+import classNames from 'classnames';
+
+const montserrat = Montserrat({
+  weight: '900',
+  subsets: ['latin'],
+});
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <Provider>
-      <Layout>
-        <Component key={router.asPath} {...pageProps} />
-      </Layout>
-      <Analytics />
-    </Provider>
+    <div className={classNames(montserrat, plusJakartaSans)}>
+      <Provider>
+        <Layout>
+          <Component key={router.asPath} {...pageProps} />
+        </Layout>
+        <Analytics />
+      </Provider>
+    </div>
   );
 }
